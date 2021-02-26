8705769a2b6684f60396f59abcf15ebe0a15c1a6
Olena Kaminska <helen.kaminskaya@gmail.com>
2021-02-26 09:06:05 +0100

    final correction

--- js/task-5.js
+++ js/task-5.js
@@ -2,11 +2,10 @@ const inputRef = document.querySelector('#name-input');
 const outputRef = document.querySelector('#name-output');
 const defaultValue = outputRef.textContent;
 
-console.log(defaultValue)
 
 inputRef.addEventListener('input', e=>{
 if (e.target.value.length === 0){
-  console.log(defaultValue)
+  
   outputRef.textContent=defaultValue;
 } else {
   outputRef.textContent = e.target.value
